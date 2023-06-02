be978ad51a0405d7f49e1eae03645ac19543aa66
adereb-web <ireoluwaadegoke7@gmail.com>
2023-06-02 05:25:47 +0100

    Work

---
+++ js2.js
@@ -0,0 +1,49 @@
+
+/*
+for (let i = 0; i <= 10; i++) {
+     console.log(i)
+ }
+ 
+
+let y = 0;
+ for (let x= 1; x <= 100; x++) {
+    y= x+y;
+    console.log(y)
+ }
+ 
+
+ let x = [10, 20, 30, 40, 50, 60, 70];
+ let y = 0;
+ for (let i=0; i < x.length; i++) {
+    y = y+x[i];
+    let z = y/x.length;
+    console.log(z)
+ }
+ 
+
+ let x = 10;
+ let y = 1;
+ for (i = 0; i <= 10; i++) {
+    y = y*(x-i);
+    console.log(y)
+ }
+ 
+
+ let x ='Blessing';
+ let y = 'Reversed x is ';
+ for (let i = x.length - 1; i >= 0; i--) {
+    y = y+ x[i];
+    console.log(y)
+ }
+ 
+let y = 1
+ for (let i = 1; i <= 100; i++) {
+    y = 
+     console.log(i)
+ }
+ */
+ let y = 1;
+ while (y < 100) {
+    y = y + 2;
+    console.log(y)
+ }
